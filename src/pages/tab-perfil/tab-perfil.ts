b7dc601bfe3109ff75aed0ac58fc226ee94468ed
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-05-04 23:24:36 -0300

    - Adicionando pagina de tab messages e perfil

--- src/pages/tab-perfil/tab-perfil.ts
+++ src/pages/tab-perfil/tab-perfil.ts
@@ -2,16 +2,18 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-tab-perfil',
-  templateUrl: 'tab-perfil.html',
+	selector: 'page-tab-perfil',
+	templateUrl: 'tab-perfil.html',
 })
 export class TabPerfil {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TabPerfil');
-  }
-
+	
+	public tab = "perfil";
+	
+	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	}
+	
+	ionViewDidLoad() {
+		console.log('ionViewDidLoad TabPerfil');
+	}
+	
 }
