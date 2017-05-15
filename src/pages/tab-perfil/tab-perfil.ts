ca482c91cc8b928d2292ddcbed699b56c8266373
rafaelodassi <rafaelodassi@hotmail.com>
2017-05-15 01:39:54 -0300

    Updates

--- src/pages/tab-perfil/tab-perfil.ts
+++ src/pages/tab-perfil/tab-perfil.ts
@@ -1,19 +1,60 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ActionSheetController, Platform } from 'ionic-angular';
 
 @Component({
 	selector: 'page-tab-perfil',
 	templateUrl: 'tab-perfil.html',
 })
-export class TabPerfil {
-	
+export class TabPerfil {	
 	public tab = "perfil";
 	
-	constructor(public navCtrl: NavController, public navParams: NavParams) {
+	constructor(public navCtrl: NavController, public navParams: NavParams, public actionsheetCtrl: ActionSheetController, public platform: Platform) {
 	}
 	
 	ionViewDidLoad() {
 		console.log('ionViewDidLoad TabPerfil');
 	}
+
+	openMenu() {
+		let actionSheet = this.actionsheetCtrl.create({
+			title: 'Ações',
+			cssClass: 'action-sheets-basic-page',
+			buttons: [
+				{
+				text: 'Editar',
+				role: 'editar',
+				icon: !this.platform.is('ios') ? 'create' : null,
+				handler: () => {
+					console.log('Delete clicked');
+				}
+				},
+				{
+				text: 'Alterar status',
+				role: 'status',
+				icon: !this.platform.is('ios') ? 'home' : null,
+				handler: () => {
+					console.log('Share clicked');
+				}
+				},								
+				{
+				text: 'Excluir',
+				role: 'excluir', // will always sort to be on the bottom
+				icon: !this.platform.is('ios') ? 'trash' : null,
+				handler: () => {
+					console.log('Cancel clicked');
+				}
+				},
+				{
+				text: 'Cancelar',
+				role: 'cancelar', // will always sort to be on the bottom
+				icon: !this.platform.is('ios') ? 'close' : null,
+				handler: () => {
+					console.log('Cancel clicked');
+				}
+				}
+			]
+		});
 	
+		actionSheet.present();
+	}	
 }
