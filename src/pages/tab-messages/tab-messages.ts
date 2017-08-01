62804d8e2eae1a63d8cb2762f24378ff63d5fb64
Willian Gomes de Oliveira <willian.goliveira@outlook.com>
2017-07-31 23:05:12 -0300

    Metodos para api de messagens

--- src/pages/tab-messages/tab-messages.ts
+++ src/pages/tab-messages/tab-messages.ts
@@ -3,22 +3,26 @@ import { App, IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Messages } from '../messages/messages';
 
 @Component({
-    selector: 'page-tab-messages',
-    templateUrl: 'tab-messages.html',
+	selector: 'page-tab-messages',
+	templateUrl: 'tab-messages.html',
 })
 
 export class TabMessages {
-    constructor(
-        public app: App,
-        public navCtrl: NavController,
-        public navParams: NavParams
-    ) {}
+	constructor(
+		public app: App,
+		public navCtrl: NavController,
+		public navParams: NavParams
+	) {}
 
-    /**
-     * Abre pagina das mensagens
-     * @param msgError: String
-    */
-    public openMessages() {
-        this.app.getRootNav().push(Messages);
-    }
+	ngOnInit() {
+		
+	}
+
+	/**
+	 * Abre pagina das mensagens
+	 * @param msgError: String
+	*/
+	public openMessages() {
+		this.app.getRootNav().push(Messages);
+	}
 }
--- src/utils/Config.ts
+++ src/utils/Config.ts
@@ -1,3 +1,4 @@
 export default {
-    api_url: "http://54.233.185.96:3000/api"
+    // api_url: "http://54.233.185.96:3000/api"
+    api_url: "http://192.168.0.17:3000/api"
 }
